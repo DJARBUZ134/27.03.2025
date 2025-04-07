6ee849ce97ad982784783ab37e8d895213721a7c
DJARBUZ134 <dojik9613@gmail.com>
2025-04-07 17:37:54 +0500

    Добавьте файлы проекта.

---
+++ 27.03.2025/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace _27._03._2025
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(303, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "СЧЁТ: 0";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Enabled = false;
+            this.richTextBox1.Location = new System.Drawing.Point(306, 25);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(106, 147);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 303);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_Keydown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ 27.03.2025/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _27._03._2025
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        int x0 = 250;
+        int y0 = 150;
+        int xt = 250;
+        int yt = 250;
+        Graphics g;
+        bool needApple = true;
+        int x = 0;
+        int y = 0;
+        int X = 0;
+        int Y = 0;
+        int l = 1;
+        int k = 1;
+        int j = 1;
+        bool needBonus = true;
+        int score = 1;
+        SolidBrush sb;
+
+       
+        Queue<Point> Tail = new Queue<Point>();
+
+        private void DrawApple(int R, int G, int B)
+        {
+            sb = new SolidBrush(Color.FromArgb(R, G, B));
+            if (needApple)
+            {
+                Random rnd = new Random();
+                x = rnd.Next(20) * 10;
+                y = rnd.Next(20) * 10;
+                needApple = false;
+            }
+            g.FillRectangle(sb, x, y, 10, 10);
+        }
+
+        private void BonusSize(int X, int Y)
+        {
+
+            sb = new SolidBrush(Color.Black);
+            if (score % 10 == 0)
+            {
+                Random rnd = new Random();
+                x = rnd.Next(20) * 10;
+                y = rnd.Next(20) * 10;
+                needBonus = false;
+            }
+            g.FillRectangle(sb, x, y, 10, 10);
+        }
+        private void Form1_Keydown(object sender, KeyEventArgs e)
+        {
+            Random rand = new Random();
+            g = pictureBox1.CreateGraphics();
+            sb = new SolidBrush(Color.Red);
+           // g.Clear(Color.AliceBlue);
+            DrawApple(l, k, j);
+            int speed = (e.Shift) ? 3 : 1;
+            // Добавление новой головы в очередь
+            Point newHead = new Point(x0, y0);
+            if (Tail.Count == 0)
+            {
+                Point PP = new Point(x0, y0);
+                Tail.Enqueue(PP);
+            }
+                switch (e.KeyCode)
+                {
+                    case Keys.A: DrawSquare(-1, 0,speed); break;
+                    case Keys.W: DrawSquare(0 , -1, speed); break;
+                    case Keys.S: DrawSquare(0 , 1, speed); break;
+                    case Keys.D: DrawSquare(1 , 0, speed); break;
+                }
+            // Проверка на выход за границы игрового поля
+            if (xt < 0 || xt >= pictureBox1.Width || yt < 0 || yt >= pictureBox1.Height)
+            {
+                GameOver(); // Конец игры
+                return;
+            }
+
+            // Проверка на столкновение со своим телом
+            
+            foreach (Point segment in Tail)
+            {
+
+                if (segment.X == xt && segment.Y == yt)
+                {
+                    GameOver(); // Конец игры
+                    return;
+                }
+            }
+
+            // Добавление новой головы
+            //Tail.Enqueue(new Point(xt, yt));
+
+
+
+
+
+
+
+            foreach (Point l in Tail)
+            {
+                richTextBox1.Text = l.X+" - "+l.Y;
+            }
+
+            if (x0 == x && y0 == y)
+            {
+                needApple = true;
+                l = rand.Next(255);
+                k = rand.Next(255);
+                j = rand.Next(255);
+                Point PP = new Point();
+                Tail.Enqueue(PP);
+                DrawApple(l, k, j);
+                label1.Text = $"СЧЁТ: {score++}";
+            }
+
+        }
+
+        public void DrawSquare(int n, int m, int speed = 1)
+        {
+            sb = new SolidBrush(Color.Red);
+            x0 += 10 * n * speed;
+            y0 += 10 * m * speed;
+            g.FillRectangle(sb, x0, y0, 10, 10);
+            Point PP = new Point(x0,y0);
+            Tail.Enqueue(PP);
+            g.FillRectangle(new SolidBrush(Color.White), Tail.Peek().X, Tail.Peek().Y, 10, 10);
+            Tail.Dequeue();
+        }
+        private void GameOver()
+        {
+            MessageBox.Show("Game Over!");
+            Application.Exit(); // Или можно перезапустить игру
+        }
+
+        //Проблема 1: при наезде на саму себя змейка рвётся, ломается, короче типичный Омск
+        //Проблема 4: При ускоренном перемещении змейка рвётся, ломается, короче типичный Омск
+        //Проблема 5: В одном из углов должен быть квадрат с рандомным цветом, и цвет квадрата(вашего) менается, когда вы на него наступаете.
+        //Проблема 6: в другом углу квадратик для смены размера вашего квадрата(на определенное кол-во шагов, например 50)
+        //Проблема 7: Наличие возможности выхода за поле (РЕШЕНО)
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+           
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
